e1fce2359ad96174c6f9edb06c0a0881459a1b6a
1k10 <77734986+app155@users.noreply.github.com>
2026-01-31 00:41:29 +0900

    add: spring security
    SecurityConfig.java +
    index.js에 axios.interceptors 로 Spring Security를 이용한 로그인 인증 추적 기능 추가
    
    App.js handleAuthChange 에 [location] 추가로 페이지이동시마다 인증확인하여 우상단 유저상태 업데이트가능하겧마

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,6 +2,33 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import axios from 'axios';
+
+axios.defaults.baseURL = 'http://localhost:8080'; 
+axios.defaults.withCredentials = true;
+
+axios.interceptors.response.use(
+    (response) => {
+      console.log("인터셉터 확인");
+      return response;
+    },
+    (error) => {
+        // 401에러일때 작동
+        if (error.response && error.response.status === 401) {
+          console.log("인터셉터 확인2");
+          alert("ログインが必要です。");
+            
+          // 로컬스토리지 정보 삭제
+          localStorage.removeItem('user'); 
+
+          window.location.href = '/login'; 
+          return new Promise(() => {});
+        }
+
+        // 그 외 에러는 그대로 반환
+        return Promise.reject(error);
+    }
+);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
